ae0bbfb1ac5031ac0dc74c1d72bf16a9ff0a454f
John <John.samson.2021@smacatholic.org>
2018-01-30 17:35:17 -0600

    Auto Commands added

--- 6843 Comp Code/src/org/usfirst/frc/team6843/robot/commands/AutoLeftSwitch.java
+++ 6843 Comp Code/src/org/usfirst/frc/team6843/robot/commands/AutoLeftSwitch.java
@@ -4,27 +4,21 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutoLeftSwitch extends CommandGroup {
 
-    public  AutoLeftSwitch() {
-    	addSequential(new ClearEncoders());
-    	addSequential(new DistDrive());
-    	addSequential(new ClearEncoders());
-    	addSequential(new DistDrive());
-    	addSequential(new ClearEncoders());	
-    	addSequential(new DistDrive());
-    	addSequential(new ClearEncoders());
-    	addSequential(new DistDrive());
-    	addSequential(new ClearEncoders());
-    	addSequential(new ZoopZoopLeft());
-    	addSequential(new ClearEncoders());
-    	addSequential(new DistDrive());
-    	addSequential(new ClearEncoders());	
-    	addSequential(new DistDriveReverse());
-    	addSequential(new ClearEncoders());
-    	addSequential(new ZoopZoopRight());
-    	addSequential(new ClearEncoders());
-    	addSequential(new DistDrive());
-    	addSequential(new ClearEncoders());
-    	addSequential(new DistDrive());
-    	addSequential(new ClearEncoders());
-    }
+    		public  AutoLeftSwitch() {
+    			addSequential(new ClearEncoders());
+    			addSequential(new DistDrive168());
+    			addSequential(new ClearEncoders());
+    			addSequential(new ZoopZoopRight());
+    			addSequential(new ClearEncoders());
+    			// Put code to raise lift to switch height here
+    			addSequential(new ClearEncoders());
+    			addSequential(new DistDrive41());
+    			addSequential(new ClearEncoders());
+    			// Put code here to push cube onto switch here
+    			addSequential(new DistDriveReverse41());
+    			addSequential(new ClearEncoders());
+    			addSequential(new ZoopZoopLeft());
+    			addSequential(new ClearEncoders());
+    			addSequential(new DistDrive100());
+    		}
 }
---
+++ 6843 Comp Code/src/org/usfirst/frc/team6843/robot/commands/AutoRightSwitch.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team6843.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutoRightSwitch extends CommandGroup {
+
+	public  AutoRightSwitch() {
+		addSequential(new ClearEncoders());
+		addSequential(new DistDrive168());
+		addSequential(new ClearEncoders());
+		addSequential(new ZoopZoopLeft());
+		addSequential(new ClearEncoders());
+		// Put code to raise lift to switch height here
+		addSequential(new ClearEncoders());
+		addSequential(new DistDrive41());
+		addSequential(new ClearEncoders());
+		// Put code here to push cube onto switch here
+		addSequential(new DistDriveReverse41());
+		addSequential(new ClearEncoders());
+		addSequential(new ZoopZoopRight());
+		addSequential(new ClearEncoders());
+		addSequential(new DistDrive100());
+	}
+}
